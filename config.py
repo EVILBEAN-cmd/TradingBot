4ba36b592d629ca6a0b85016b79cffe24e59792c
Sinan <evilbeancmd@gmail.com>
2021-08-13 14:49:43 +0200

    Added config

---
+++ config.py
@@ -0,0 +1,6 @@
+# Secret tokens for Alpaca Api 
+# Replace these with your API connection info from the dashboard
+
+Endpoint = 'Endpoint'
+APIKeyID = 'KeyID'
+SecretKey = 'SecretKey'
---
+++ main.py
@@ -0,0 +1,8 @@
+import alpaca_trade_api as tradeapi
+import requests
+import time
+import numpy as np
+from config import *
+
+
+print(Endpoint)
